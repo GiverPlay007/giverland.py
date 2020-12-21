56633f8ad5810205fbee7dd1cf982078f1070260
GiverPlay007 <play.giver.007@gmail.com>
2020-12-21 14:43:41 -0300

    Mover cubo para um arquivo próprio

---
+++ givercity/cube.py
@@ -0,0 +1,38 @@
+from OpenGL.GL import *
+
+
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
+
+
+def cube():
+    glBegin(GL_LINES)
+
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+
+    glEnd()
--- givercity/givercity.py
+++ givercity/givercity.py
@@ -1,44 +1,11 @@
 import pygame
+
+from cube import cube
+
 from pygame.locals import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
-
-vertices = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-)
-
-edges = (
-    (0, 1),
-    (0, 3),
-    (0, 4),
-    (2, 1),
-    (2, 3),
-    (2, 7),
-    (6, 3),
-    (6, 4),
-    (6, 7),
-    (5, 1),
-    (5, 4),
-    (5, 7)
-)
-
-
-def cube():
-    glBegin(GL_LINES)
-
-    for edge in edges:
-        for vertex in edge:
-            glVertex3fv(vertices[vertex])
-
-    glEnd()
 
 
 def main():
